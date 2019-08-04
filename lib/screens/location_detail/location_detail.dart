56408d853b412a91f13d165be5f2cd2e117df5e3
juleshwar.babu <juleshwar.babu@zohocorp.com>
2019-08-04 16:56:52 +0530

    Changes 1. Added a text_section to show title and description 2. Added an image_banner to show images 3. Added style.dart for text styles

--- lib/screens/location_detail/location_detail.dart
+++ lib/screens/location_detail/location_detail.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:tourist_places/screens/location_detail/image_banner.dart';
+import 'text_section.dart';
 
 class LocationDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("title"),
+        title: Text("Marvel Superheroes"),
+        backgroundColor: Colors.black,
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Text("A"),
-          Text("b"),
-          Text('c')
+          ImageBanner("assets/images/thanos.jpg"),
+          TextSection("Guy #1", "Lorem ipusm jashd kja sdkjh aksjdh  liasdj llasj dlkajs dlja slkdj lasj lasj "),
+          TextSection("Guy #1", "Lorem ipusm jashd kja sdkjh aksjdh  liasdj llasj dlkajs dlja slkdj lasj lasj "),
+          TextSection("Guy #1", "Lorem ipusm jashd kja sdkjh aksjdh  liasdj llasj dlkajs dlja slkdj lasj lasj "),
+          TextSection("Guy #1", "Lorem ipusm jashd kja sdkjh aksjdh  liasdj llasj dlkajs dlja slkdj lasj lasj ")
         ],
       )
     );
---
+++ lib/style.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+const TEXT_SIZE_LARGE = 26.0;
+const TEXT_SIZE_MEDIUM = 20.0;
+const TEXT_SIZE_REGULAR = 16.0;
+
+const String DEFAULT_FONT = "Montserrat";
+
+const TextStyle_AppBar = TextStyle(
+    fontFamily: DEFAULT_FONT,
+    fontWeight: FontWeight.w300,
+    fontSize: TEXT_SIZE_REGULAR,
+    color: Colors.white);
+
+const TextStyle_TitleText = TextStyle(
+    fontFamily: DEFAULT_FONT,
+    fontWeight: FontWeight.w300,
+    fontSize: TEXT_SIZE_LARGE,
+    color: Colors.black);
+
+const TextStyle_Body1Text = TextStyle(
+    fontFamily: DEFAULT_FONT,
+    fontWeight: FontWeight.w300,
+    fontSize: TEXT_SIZE_REGULAR,
+    color: Colors.black);
